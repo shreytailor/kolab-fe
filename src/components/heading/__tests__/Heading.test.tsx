d805152c44e40ac2dee87347d618a9d1241eaf82
shreytailor <shreym.tailor@gmail.com>
2020-09-17 17:15:36 +1200

    tests to typescript. :white_check_mark:

--- src/components/heading/__tests__/Heading.test.tsx
+++ src/components/heading/__tests__/Heading.test.tsx
@@ -3,6 +3,6 @@ import Heading from '../Heading';
 import renderer from 'react-test-renderer';
 
 it("matches snapshot", function () {
-    const tree = renderer.create(<Heading></Heading>).toJSON();
+    const tree = renderer.create(<Heading string="Testing" fontsize="1rem"></Heading>).toJSON();
     expect(tree).toMatchSnapshot();
 })
